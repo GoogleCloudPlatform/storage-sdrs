525da7edcd12fa9984f2ec29dd952c6dfde31725
salguerod <salguerod@google.com>
2019-01-18 13:06:36 -0800

    refactored main package name to com.google.gcs.sdrs

---
+++ src/test/java/com/google/gcs/sdrs/controller/mapper/exception/JsonParseExceptionMapperTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2019 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the License.
+ *
+ * Any software provided by Google hereunder is distributed “AS IS”,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, and is not intended for production use.
+ *
+ */
+
+package com.google.gcs.sdrs.controller.mapper.exception;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.google.gcs.sdrs.controller.mapper.exception.JsonParseExceptionMapper;
+import com.google.gcs.sdrs.controller.pojo.response.ErrorResponse;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import static junit.framework.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+public class JsonParseExceptionMapperTest {
+
+  private JsonParseExceptionMapper mapper;
+
+  @Before
+  public void setUp() {
+    mapper = new JsonParseExceptionMapper();
+  }
+
+  @Test
+  public void toResponseReturnsResponseWithFieldsSet() {
+    JsonParseException exceptionMock = mock(JsonParseException.class);
+
+    Response response = mapper.toResponse(exceptionMock);
+
+    assertEquals(response.getStatus(), 400);
+    assertTrue(((ErrorResponse) response.getEntity()).getMessage().contains("Unable"));
+  }
+
+  @Test
+  public void createExceptionResponseMessageReturnsSomething() {
+    String message = mapper.createExceptionResponseMessage(null);
+
+    assertTrue(message.contains("JSON"));
+  }
+}
---
+++ src/test/java/com/google/gcs/sdrs/worker/WorkerLogTest.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2019 Google LLC. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the “License”);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and limitations under the License.
+ *
+ * Any software provided by Google hereunder is distributed “AS IS”,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, and is not intended for production use.
+ *
+ */
+
+package com.google.gcs.sdrs.worker;
+
+import java.util.UUID;
+
+import org.joda.time.DateTime;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.gcs.sdrs.worker.WorkerLog;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+/**
+ * Test class for WorkerLog
+ */
+public class WorkerLogTest {
+  private WorkerLog objectUnderTest;
+
+  /**
+   * Set up steps before each test
+   */
+  @Before
+  public void setUp() {
+    objectUnderTest = new WorkerLog();
+  }
+
+  /**
+   * Tear down steps after each test
+   */
+  @After
+  public void tearDown() {
+    objectUnderTest = null;
+  }
+
+  /**
+   * Tests that the constructor sets the ID as a UUID
+   */
+  @Test
+  public void workerLogConstructorTest() {
+    try{
+      UUID workerLogId = UUID.fromString(objectUnderTest.getId());
+      assertNotNull(workerLogId);
+    } catch (IllegalArgumentException ex) {
+      fail("The generated WorkerLog ID is not a UUID.");
+    }
+  }
+
+  /**
+   * Tests that properties read back correctly after being set
+   */
+  @Test
+  public void workerLogPropertiesTest() {
+    WorkerLog.WorkerLogStatus testStatus = WorkerLog.WorkerLogStatus.RUNNING;
+    String testType = "testType";
+    DateTime testStartTime = DateTime.now().minusHours(1);
+    DateTime testEndTime = DateTime.now();
+
+    objectUnderTest.setStatus(testStatus);
+    objectUnderTest.setType(testType);
+    objectUnderTest.setStartTime(testStartTime);
+    objectUnderTest.setEndTime(testEndTime);
+
+    assertEquals(testStatus, objectUnderTest.getStatus());
+    assertEquals(testType, objectUnderTest.getType());
+    assertEquals(testStartTime, objectUnderTest.getStartTime());
+    assertEquals(testEndTime, objectUnderTest.getEndTime());
+  }
+
+  /**
+   * Tests the toString method to ensure null values display properly
+   */
+  @Test
+  public void workerLogToStringTest() {
+    assertNotNull(objectUnderTest.toString());
+    assertTrue(objectUnderTest.toString().length() > 0);
+
+    objectUnderTest.setType("testType");
+    assertNotNull(objectUnderTest.toString());
+    assertTrue(objectUnderTest.toString().length() > 0);
+
+    objectUnderTest.setStatus(WorkerLog.WorkerLogStatus.RUNNING);
+    assertNotNull(objectUnderTest.toString());
+    assertTrue(objectUnderTest.toString().length() > 0);
+
+    objectUnderTest.setStartTime(DateTime.now().minusHours(1));
+    assertNotNull(objectUnderTest.toString());
+    assertTrue(objectUnderTest.toString().length() > 0);
+
+    objectUnderTest.setEndTime(DateTime.now());
+    assertNotNull(objectUnderTest.toString());
+    assertTrue(objectUnderTest.toString().length() > 0);
+  }
+}
