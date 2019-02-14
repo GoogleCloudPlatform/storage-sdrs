1b3b032f1d509c39095b438d042398f4127f1130
Jordan Kersten <jordank@slalom.com>
2019-02-14 14:54:16 -0800

    Removing DatasetName from Index
    Changing index to include only 2 values: data_storage_name and project_id. Adding logic in the mod script to conditionally remove old 3 value index if it exists.

--- scripts/sql/mods/00_add_retention_rule_business_key.sql
+++ scripts/sql/mods/00_add_retention_rule_business_key.sql
@@ -6,16 +6,30 @@ DELIMITER //
 
 CREATE PROCEDURE modify_sdrs_add_rule_composite_key ( )
 BEGIN
+  DECLARE oldIndexExists INT;
   DECLARE indexExists INT;
-  SELECT COUNT(*) INTO indexExists
+
+  -- Drop the old 3 value index if it still exists
+  SELECT COUNT(*) INTO oldIndexExists
   FROM INFORMATION_SCHEMA.STATISTICS
   WHERE table_schema=DATABASE()
     AND table_name='retention_rule'
     AND index_name='unique_dataset_storage_project';
 
   IF indexExists = 0 THEN
+    DROP INDEX unique_dataset_storage_project on retention_rule;
+  END IF;
+
+  -- Create new 2 value index if it doesn't exist
+  SELECT COUNT(*) INTO indexExists
+  FROM INFORMATION_SCHEMA.STATISTICS
+  WHERE table_schema=DATABASE()
+    AND table_name='retention_rule'
+    AND index_name='unique_storage_project';
+
+  IF indexExists = 0 THEN
     ALTER TABLE retention_rule
-      ADD UNIQUE KEY `unique_dataset_storage_project` (`dataset_name`, `data_storage_name`, `project_id`);
+    ADD UNIQUE KEY `unique_storage_project` (`data_storage_name`, `project_id`);
   END IF;
 END//
 
