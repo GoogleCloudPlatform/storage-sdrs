053cbbe8ada500894b3ccfb735d5ec9bc4d0377c
Tom Flenniken <tom.flenniken@slalom.com>
2019-02-21 14:48:08 -0800

    Extracted JWT behavior/self request to a utility class.

--- src/main/java/com/google/gcs/sdrs/runners/ValidationRunner.java
+++ src/main/java/com/google/gcs/sdrs/runners/ValidationRunner.java
@@ -17,19 +17,7 @@
 
 package com.google.gcs.sdrs.runners;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTCreator;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.util.Utils;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.json.JsonFactory;
-import java.io.IOException;
-import java.security.interfaces.RSAPrivateKey;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
+import com.google.gcs.sdrs.util.SdrsRequestClientUtil;
 import org.apache.commons.configuration2.Configuration;
 import org.apache.commons.configuration2.builder.fluent.Configurations;
 import org.apache.commons.configuration2.ex.ConfigurationException;
@@ -45,49 +33,13 @@ public class ValidationRunner implements Runnable {
   public void run() {
     try {
       Configuration config = new Configurations().xml("applicationConfig.xml");
-      String serviceUrl = config.getString("scheduler.serviceUrl");
-      String serviceProtocol = config.getString("scheduler.serviceProtocol");
+      String endpoint = config.getString("scheduler.validationService.endpoint");
 
-      String jwt = generateJwt(String.format("https://%s", serviceUrl));
-      logger.info(String.format("JWT generated: %s", jwt));
-
-      // TODO: This URL should be the load balancer URL
       logger.info("Making request to validation service endpoint.");
-      Client client = ClientBuilder.newClient();
-      client
-          .target(String.format("%s://%s", serviceProtocol, serviceUrl))
-          .path("events/validation")
-          .request()
-          .header("Authorization", String.format("Bearer %s", jwt))
-          .post(null);
+      SdrsRequestClientUtil.request(endpoint).post(null);
 
     } catch (ConfigurationException ex) {
       logger.error(String.format("Configuration file could not be read: %s", ex.getMessage()));
-    } catch (IOException ex) {
-      logger.error(String.format("Unable to generate JWT: %s", ex.getMessage()));
     }
-  }
-
-  private static String generateJwt(String audience) throws IOException {
-    HttpTransport httpTransport = Utils.getDefaultTransport();
-    JsonFactory jsonFactory = Utils.getDefaultJsonFactory();
-    GoogleCredential cred = GoogleCredential.getApplicationDefault(httpTransport, jsonFactory);
-
-    Date now = new Date();
-    Date expTime = new Date(System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(3600));
-
-    JWTCreator.Builder token =
-        JWT.create()
-            .withIssuedAt(now)
-            .withExpiresAt(expTime)
-            .withIssuer(cred.getServiceAccountId())
-            .withSubject(cred.getServiceAccountId())
-            .withAudience(audience)
-            .withClaim("email", cred.getServiceAccountId());
-
-    // Sign the JWT with a service account
-    RSAPrivateKey key = (RSAPrivateKey) cred.getServiceAccountPrivateKey();
-    Algorithm algorithm = Algorithm.RSA256(null, key);
-    return token.sign(algorithm);
   }
 }
