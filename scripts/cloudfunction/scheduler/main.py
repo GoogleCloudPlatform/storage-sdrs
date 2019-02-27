f4e2fb7849384e781bbb288d4c54c0fbda6058d1
salguerod <salguerod@google.com>
2019-02-27 11:21:08 -0800

    updating else if logic to cloud function

--- scripts/cloudfunction/scheduler/main.py
+++ scripts/cloudfunction/scheduler/main.py
@@ -1,36 +1,21 @@
-import base64
-import os
-import requests
- 
-import urllib.parse
-
-import flask
-
 def scheduler_pubsub(pub_sub_event, context):
-    """Triggered from a message on a Cloud Pub/Sub topic.
-    Args:
-         pub_sub_event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the pub_sub_event.
-    """
-    pub_sub_message = base64.b64decode(pub_sub_event['data']).decode('utf-8')
-    print(pub_sub_message)
+    
     project = os.environ.get('GCP_PROJECT', 'Specified environment variable is not set.')
     print(f"Project : {project}")
-   
-   	# only need type - target and projectId parameters are optional
+    print(f"Processing pub_sub_event: {pub_sub_event['name']}.")
+    pub_sub_message = base64.b64decode(pub_sub_event['data']).decode('utf-8')
+    print(f"Pubsub message : {pub_sub_message}")
     type='POLICY'
-    dict_to_send = {'type': 'DATASET'}
+    print ('data:','invoking REST call')
+    dict_to_send = {'type':type}
   
-    print("debug here in 02/26/2019")
+    print(" test 1099 ")
     
     if pub_sub_message == 'executor':
-    	print ('Invoking the executor')
-    	response = requests.post('http://sdrs-api.endpoints.sdrs-server.cloud.goog:80/events/execution/', json=dict_to_send)
-    	dict_from_server = get_response.json()
+        print ('Invoking the executor')
+        response = requests.post('http://sdrs-api.endpoints.sdrs-server.cloud.goog:80/events/execution/', json=dict_to_send)
+        print ('Response from server: {}'.format(response.text))
     elif pub_sub_message == 'validator':
-    	print('Invoking the validator')
-		response = requests.post('http://sdrs-api.endpoints.sdrs-server.cloud.goog:80/events/validation/')
-    else:
-    	print('invalid event payload - no work to do')
-    	
-  
+        print('Invoking the validator')
+        response = requests.post('http://sdrs-api.endpoints.sdrs-server.cloud.goog:80/events/validation/')
+        print ('Response from server: {}'.format(response.text))
