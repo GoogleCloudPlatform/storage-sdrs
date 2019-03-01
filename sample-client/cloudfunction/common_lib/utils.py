7291569ae03b24cb8dd057c6b58c7e2ef14bae57
Matt <matt@geneau.me>
2019-03-01 13:55:14 -0800

    Updating scheduler function.

--- sample-client/cloudfunction/common_lib/utils.py
+++ sample-client/cloudfunction/common_lib/utils.py
@@ -1,3 +1,20 @@
+# Copyright 2019 Google LLC. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may not
+# use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+# Any software provided by Google hereunder is distributed "AS IS", WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, and is not intended for production use.
+
 import json
 import os
 import time
@@ -11,8 +28,10 @@ _SERVICE_ACCOUNT_EMAIL = os.getenv('FUNCTION_IDENTITY')
 PROJECT_ID = os.getenv('projectId')
 RETENTION_RULES_ENDPOINT = '{}:80/retentionrules'.format(_ENDPOINT)
 EVENTS_ENDPOINT = '{}:80/events'.format(_ENDPOINT)
-EXECUTION_ENDPOINT = '{}:80/events/execution'.format(_ENDPOINT)
-VALIDATION_ENDPOINT = '{}:80/events/validation'.format(_ENDPOINT)
+EVENTS_EXECUTION_ENDPOINT = '{}/execution'.format(EVENTS_ENDPOINT)
+EVENTS_VALIDATION_ENDPOINT = '{}/validation'.format(EVENTS_ENDPOINT)
+EVENTS_NOTIFICATION_ENDPOINT = '{}/notification'.format(EVENTS_ENDPOINT)
+
 JWT = None
 
 
