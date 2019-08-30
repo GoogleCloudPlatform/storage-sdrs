4e273a822df93eaa68c6009cd175f19858bb0909
Daniel Ardiwinata <daniel.ardiwinata@weareproof.com>
2019-08-30 06:28:26 -0700

    fix typo

--- scripts/sql/retention_schema.sql
+++ scripts/sql/retention_schema.sql
@@ -108,21 +108,22 @@ CREATE TABLE `DMQueueTable` (
   `updated_at` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
   `data_storage_name` varchar(256) NOT NULL,
   `status` varchar(256) DEFAULT NULL,
-  `priority` int(10) NOT NULL DEFAULT 0,
-  `data_storage_root varchar(256)` NOT NULL,
-  `retention_job_id` , int(10),
+  `priority` int(10) unsigned NOT NULL DEFAULT 0,
+  `data_storage_root` varchar(256) NOT NULL,
+  `retention_job_id` int(10) unsigned,
   `number_of_retry` int(10) NOT NULL DEFAULT 0,
   PRIMARY KEY (`id`),
   FOREIGN KEY (retention_job_id) REFERENCES retention_job(id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `DistributedLockEntry` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT`,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `lockIdVerificationToken` varchar(256) NOT NULL,
   `lockDuration` int(10) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 
 
