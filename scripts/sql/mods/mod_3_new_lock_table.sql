e3ca16e34445fe5e7f8e7b50793c436c9f9ddcac
Daniel Ardiwinata <daniel.ardiwinata@weareproof.com>
2019-09-01 07:06:41 -0700

    fix typo sql statment

--- scripts/sql/mods/mod_3_new_lock_table.sql
+++ scripts/sql/mods/mod_3_new_lock_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `DistributedLockEntry` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT`,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `lockIdVerificationToken` varchar(256) NOT NULL,
   `lockDuration` int(10) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
