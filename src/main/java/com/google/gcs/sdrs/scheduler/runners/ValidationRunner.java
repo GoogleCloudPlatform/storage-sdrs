34adf2025cdda7947e728783458eb7f93dc66e0c
ericshen <ericshen@google.com>
2019-09-09 13:47:28 -0700

    Deprecate Execution and Validation scheduler, and create DM processing worker.

--- src/main/java/com/google/gcs/sdrs/scheduler/runners/ValidationRunner.java
+++ src/main/java/com/google/gcs/sdrs/scheduler/runners/ValidationRunner.java
@@ -27,6 +27,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /** Calls the Validation service endpoint when run */
+@Deprecated
 public class ValidationRunner implements Runnable {
 
   private static final Logger logger = LoggerFactory.getLogger(ValidationRunner.class);
